51ec2ba1bac19360d41173599da2d9a42ed82b11
suraj-mane <sun.mane1100@gmail.com>
2022-07-05 11:23:29 +0530

    task is complete

--- block-BRaaax/button/src/Button.js
+++ block-BRaaax/button/src/Button.js
@@ -1,11 +1,15 @@
+import { FaAngleDown } from "react-icons/fa";
 function Button (props) {
     return (
         <div className="container text-center mt-5">
-            <button className="btn btn-primary m-3 d-block">{props.lebal}</button>
-            <button className="btn btn-primary m-3 d-block">{props.lebal}</button>
-            <button className="btn btn-primary m-3 d-block">{props.lebal}</button>
-            <button className="btn btn-primary m-3 d-block">{props.lebal}</button>
-            <button className="btn btn-primary m-3 d-block">{props.lebal}</button>
+            <button className=" m-3 d-block btn1">{props.lebal}</button>
+            <button className="btn m-3 d-block btn2"><FaAngleDown />{props.lebal}</button>
+            <button className="btn m-3 d-block btn3">{props.lebal}</button>
+            <button className="btn m-3 d-block btn4">{props.lebal}</button>
+            <button className="btn m-3 d-block btn5">{props.lebal}</button>
+            <button className="btn m-3 d-block btn6">{props.lebal}</button>
+            <button className="btn m-3 d-block btn7">{props.lebal}</button>
+            <button className="btn m-3 d-block btn8">{props.lebal}</button>
         </div>
     )
 }
